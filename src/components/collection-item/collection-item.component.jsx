d7d2eac88e6a351e5a49e749383770d011c64b13
zakariae <zakariae@example.com>
2019-07-27 08:08:38 +0200

    Adding header component and link to home

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -7,10 +7,10 @@ const CollectionItem = ({id, name, price, imageUrl}) => (
         <div 
         className='image'
         style={{backgroundImage: `url(${imageUrl})`}} />
-            <div className='collection-footer'>
-                <span className='name'>{name}</span>
-                <span className='price'>{price}</span>
-            </div>
+        <div className='collection-footer'>
+            <span className='name'>{name}</span>
+            <span className='price'>{price}</span>
+        </div>
     </div>
 )
 
