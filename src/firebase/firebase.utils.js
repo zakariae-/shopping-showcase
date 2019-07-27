1804ae06d8968ec50f0e73053663f57f50fcd5f8
zakariae <zakariae@example.com>
2019-07-27 20:08:52 +0200

    Refacto

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,13 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyDY0rCNZNZQuNlbahrpoyAaFAmV5yxDOSo",
-    authDomain: "crwn-db-8bc6c.firebaseapp.com",
-    databaseURL: "https://crwn-db-8bc6c.firebaseio.com",
-    projectId: "crwn-db-8bc6c",
+    apiKey: "xxxx",
+    authDomain: "xxx",
+    databaseURL: "xxx",
+    projectId: "xx",
     storageBucket: "",
-    messagingSenderId: "799273656673",
-    appId: "1:799273656673:web:85dda45d5236e799"
+    messagingSenderId: "xx",
+    appId: "xx"
   };
 
 firebase.initializeApp(config);
