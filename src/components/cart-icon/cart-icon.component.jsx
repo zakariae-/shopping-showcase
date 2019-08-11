9dca2c2a17fca9312c40ab3ddb1c10524f46d39e
zakariae <zakariae@example.com>
2019-08-11 20:21:44 +0200

    Use createStructuredSelector in all project and create new selectors in user and cart

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {createStructuredSelector} from 'reselect'
 
 import {toggoleCartHidden} from '../../redux/cart/cart.actions'
 import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg'
@@ -18,8 +19,8 @@ const mapDispachToProps = dispach => ({
     toggoleCartHidden: () => dispach(toggoleCartHidden())
 });
 
-const mapStateToProps = (state) => ({
-    itemCount: selectCartItemsCount(state)
+const mapStateToProps = createStructuredSelector({
+    itemCount: selectCartItemsCount
 });
 
 export default connect(
